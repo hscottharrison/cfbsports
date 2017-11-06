c2d03593944d14749b0adf2ec129d5933bdcbcc2
Hunter Harrison <hunter@Hunters-MacBook-Pro.local>
2017-11-06 17:31:40 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from './components/UIComponents/navBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
